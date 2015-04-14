2d209f1f1013e326b63b096bbdff49c19297fa7b
usuario <usuario@LAB02-16>
2015-04-14 12:58:53 -0600

    HOLA MUNDO

--- src/ejemplos/archivos/Registro.java
+++ src/ejemplos/archivos/Registro.java
@@ -31,7 +31,7 @@ public class Registro extends JFrame {
         jlnombre = new JLabel("Nombre");
         jtsalario = new JTextField(10);
         jlsalario = new JLabel("Salario");
-
+//HOLA MUNDO
         JPanel informacion = new JPanel();
         informacion.add(jlnombre);
         informacion.add(jtnombre);
