d79911cdbcb49086bdf7659d67196da67117b8fa
usuario <usuario@LAB02-16>
2015-04-14 12:44:39 -0600

    ACTUALIZACION System3

--- src/ejemplos/archivos/Registro.java
+++ src/ejemplos/archivos/Registro.java
@@ -37,7 +37,7 @@ public class Registro extends JFrame {
         informacion.add(jtnombre);
         informacion.add(jlsalario);
         informacion.add(jtsalario);
-        System.out.println("2");
+        System.out.println("3");
         iniciador = 1;
         salvar.addActionListener(
                 new ActionListener() {
